cc9ae681d7f620255e1aa4d97e4cec074c80af04
Alexandre LEGER <aleger@student.42.fr>
2014-04-30 15:57:27 +0200

    Moving ldap_search on ldap_server module

--- auth42.py
+++ auth42.py
@@ -1,5 +1,4 @@
 import base64
-import ldap
 from ldap_server import LdapServer
 
 
@@ -23,16 +22,6 @@ class Auth42(LdapServer):
             response = False
 
         return response
-
-    def ldap_search(self, user):
-        """
-        Tries to search for given user -> Dict
-        """
-        dn = "uid=" + user + ",ou=2013,ou=people,dc=42,dc=fr"
-        try:
-            return self.server.search_s(dn, ldap.SCOPE_SUBTREE)[0][1]
-        except ldap.LDAPError:
-            return None
 
     def ldap_get_email(self, user):
         """
--- ldap_server.py
+++ ldap_server.py
@@ -28,3 +28,13 @@ class LdapServer(object):
         Close connection with ldap server
         """
         self.server.unbind()
+
+    def ldap_search(self, user):
+        """
+        Tries to search for given user -> Dict
+        """
+        dn = "uid=" + user + ",ou=2013,ou=people,dc=42,dc=fr"
+        try:
+            return self.server.search_s(dn, ldap.SCOPE_SUBTREE)[0][1]
+        except ldap.LDAPError:
+            return None
